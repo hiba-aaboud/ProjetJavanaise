8dbf1d627f051b1d41a8ac819946ef4f7f08828e
Bahae <bahaeddine_moutaouikil@um5.ac.ma>
2025-11-03 10:36:09 +0100

    Re organize Repo

--- SOURCES-JVN/src/jvn/MySharedInterface.java
+++
@@ -1,9 +0,0 @@
-package jvn;
-
-import annotations.JvnRead;
-import annotations.JvnWrite;
-
-public interface MySharedInterface {
-
-	@JvnRead String getValue();
-    @JvnWrite void setValue(String v);}
